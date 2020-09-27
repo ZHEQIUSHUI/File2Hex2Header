43f829194468f802cd3f7ecd3c78417d9c93585b
Arno <46700201+ZHEQIUSHUI@users.noreply.github.com>
2020-09-27 08:48:06 +0800

    Update File2Hex2Header.h

--- File2Hex2Header.h
+++ File2Hex2Header.h
@@ -5,6 +5,45 @@
 #include <sstream>
 #include <fstream>
 
+template <class _Ty, size_t _Size>
+constexpr size_t countof(const _Ty(&)[_Size]) noexcept {
+	return _Size;
+}
+
+void string_replace(std::string& strBig, const std::string& strsrc, const std::string& strdst)
+{
+	std::string::size_type pos = 0;
+	std::string::size_type srclen = strsrc.size();
+	std::string::size_type dstlen = strdst.size();
+
+	while ((pos = strBig.find(strsrc, pos)) != std::string::npos)
+	{
+		strBig.replace(pos, srclen, strdst);
+		pos += dstlen;
+	}
+}
+
+void GetPathOrURLShortName(std::string strFullName, std::string& filename, std::string& name, std::string& suffix_str)
+{
+	if (strFullName.empty())
+	{
+		return;
+	}
+
+	string_replace(strFullName, "/", "\\");
+	//1.获取不带路径的文件名
+	std::string::size_type iPos = strFullName.find_last_of('\\') + 1;
+	filename = strFullName.substr(iPos, strFullName.length() - iPos);
+
+	//2.获取不带后缀的文件名
+	name = filename.substr(0, filename.rfind("."));
+
+	//3.获取后缀名
+	suffix_str = filename.substr(filename.find_last_of('.') + 1);
+
+}
+
+
 std::string readfile(std::string filename)
 {
 	std::fstream in(filename.c_str(), std::ios::in | std::ios::binary);
@@ -38,7 +77,7 @@ void writefile(std::string ctx, std::string filename)
 	out.close();
 }
 
-std::string Str2hex(std::string ctx)
+std::string Str2hex(std::string ctx,std::string _name)
 {
 	std::stringstream result;
 	result << "#ifdef _MSC_VER" << std::endl;
@@ -46,7 +85,8 @@ std::string Str2hex(std::string ctx)
 	result << "#else" << std::endl;
 	result << "__attribute__((aligned(4)))" << std::endl;
 	result << "#endif" << std::endl;
-	result << "static const unsigned char buf[]={" << std::endl;
+	result << "//constexpr int bufsize = " << ctx.size() << ";" << std::endl;
+	result << "static const unsigned char "<< _name <<"[] = {" << std::endl;
 	for (size_t i = 0; i < ctx.length(); i++)
 	{
 		if (i != 0 && i % 20 == 0)
@@ -63,8 +103,10 @@ std::string Str2hex(std::string ctx)
 
 void File2Header(std::string filename,std::string headerfile)
 {
+	std::string _filename, _name, _suffix_str;
+	GetPathOrURLShortName(filename, _filename, _name, _suffix_str);
 	auto ctx = readfile(filename);
-	auto hex = Str2hex(ctx);
+	auto hex = Str2hex(ctx, _name);
 	std::fstream out(headerfile.c_str(), std::ios::out | std::ios::binary);
 
 	if (out.is_open())
